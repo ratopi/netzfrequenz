217268e75f77865c50d0922a9f3bee920b0b307b
Ralf Th. Pietsch <ratopi@abwesend.de>
2022-01-23 22:55:14 +0100

    changes from radulfur

--- netzfrequenz.ino
+++ netzfrequenz.ino
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 /**
   * Bestimmung der Netzfrequenz
   * Copyright 2015 by Ralf Th. Pietsch <ratopi@abwesend.de>
@@ -6,76 +8,116 @@
   
 #define PROBE_COUNT 50
 #define MICROS 1000000.
-// #define KORREKTUR 1.001341341
-#define KORREKTUR 1.
+#define KORREKTUR 1.0
+#define SecSamples 100
 
 #define DEBUG 0
 
-int min;
-int max;
-int val;
-int i, j;
-float freq;
+int Vmin;
+int Vmax;
+int Vval;
+int Vmid;
 
+int i, j;
+float freq, freqmin, freqmax;
 long start;
 long end;
 
+//#############################################################################
+//  Function declarations
+//#############################################################################
+
+void period();
+int analogReadPlus() ;
+
+//#############################################################################
+//  Arduino code
+//#############################################################################
+
 void setup()
 {  
-  Serial.begin( 9600 );
+  Serial.begin( 115200 );
 
-  analogReference( INTERNAL );
+  //analogReference( INTERNAL );
   
   Serial.println( "reading 10000 samples ..." );
   
   for ( i = 0; i < 10000; i++ )
   {
-      analogRead( A0 );
+      Vval = analogRead( A0 );
+      //printf("%d V\n",Vval);
+      Vmin = min( Vmin, Vval);
+      Vmax = max( Vmax, Vval);
   }
   
+  Vmid = ( Vmax - Vmin ) / 2;
+
+  Serial.printf("Min: %d, Max: %d\n",Vmin,Vmax);
   Serial.println( "waiting for first period");
   
   period();
 
-  end = micros();
+  //end = micros();
   
   Serial.println( "starting..." );
 }
 
 void loop()
 {
-  start = end;
-  min = 1024;
-  max = 0;
+  Vmin = 1024;
+  Vmax = 0;
+  float Delta;
+
+  while ( analogReadPlus() == 0 );
+  for ( j = 0; j < SecSamples; j++ ) {
+    analogReadPlus();
+  }
+  while ( analogReadPlus() > 0 );
+
+  start = micros();
+   
   for ( i = 0; i < PROBE_COUNT; i++ )
   {
     period();
   }
   end = micros();
-  freq = ( ( PROBE_COUNT * MICROS * KORREKTUR ) / ( end - start ) );
+  freq = ( PROBE_COUNT * ( MICROS / double( end - start ) ) );
+
+  Delta = abs(freq - 50.0);
+
   if ( DEBUG ) 
   {
-    Serial.print( min );
+    Serial.print( Vmin );
     Serial.print( " " );
-    Serial.print( max );
+    Serial.print( Vmax );
     Serial.print( " " );
   }
-  Serial.println( freq, 3 );
+  switch( Delta > 0.2 ? 2 : Delta > 0.02 ? 1 : 0 ){
+    case 2: Serial.print("Error: ");break;
+    case 1: Serial.print("Warning: ");break;
+    }
+
+  Serial.printf("Count: %d, Micros: %f, Frequency: %f\n",PROBE_COUNT,MICROS,freq);
 }
 
 void period()
 {
-  while ( analogReadPlus() > 50 );
-  while ( analogReadPlus() < 100 );
+  while ( analogReadPlus() == 0 );
+  for ( j = 0; j < SecSamples; j++ ) {
+    analogReadPlus();
+  }
+  while ( analogReadPlus() > 0 );
 }
 
 int analogReadPlus() 
 {
-  val = analogRead( A0 );
+  Vval = analogRead( A0 );
   
-  if ( val < min ) min = val;
-  if ( val > max ) max = val;
-  
-  return val;
+  if ( DEBUG ) 
+  {
+    Vmin = min( Vmin, Vval );
+    Vmax = max( Vmax, Vval );
+  }
+ 
+  return Vval;
 }
-
